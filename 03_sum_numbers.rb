873130009bea2cf985b096528a6d3fd5f2e1c26f
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-10-03 08:58:34 +1000

    day 3

---
+++ 03_sum_numbers.rb
@@ -0,0 +1,22 @@
+# Sum Numbers
+#
+# Write a method that will take an array of numbers,
+# and return their sum.
+#
+# Difficulty:
+# 3/10
+#
+# Example:
+# sum_numbers([1,1,1]) should return 3
+# sum_numbers([5,2,100,0,10]) should return 117
+#
+# Check your solution by running the tests:
+# ruby tests/03_sum_numbers_test.rb
+#
+
+def sum_numbers (numbers)
+  numbers.inject :+
+
+end
+sum_numbers([1, 1, 1])
+sum_numbers([5, 2 , 100, 0, 10])
