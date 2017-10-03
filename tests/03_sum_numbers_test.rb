746ef2998a18b7715eb2b01e862f7e19a8db0b5a
Your Name <renzo-lim@hotmail.com>
2017-10-03 10:20:14 +1000

    sum number method added and tested

--- tests/03_sum_numbers_test.rb
+++ tests/03_sum_numbers_test.rb
@@ -2,7 +2,7 @@ require 'test/unit'
 require_relative '../03_sum_numbers'
 
 class LargestNumberTest < Test::Unit::TestCase
-  def test_sum_numbes
+  def test_sum_numbers
     assert_equal(3, sum_numbers([1,1,1]))
     assert_equal(117, sum_numbers([5,2,100,0,10]))
   end
