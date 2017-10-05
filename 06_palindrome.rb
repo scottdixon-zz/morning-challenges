0f4a89f0f62fc2948078b237f03b9aebb8e08de6
Scott Dixon <scott@howrefreshing.com.au>
2017-10-06 08:09:41 +1000

    06 - Palindrome

---
+++ 06_palindrome.rb
@@ -0,0 +1,20 @@
+# Palindrome
+#
+# Write a method that will take a word as a string and
+# return a boolean indicating whether or not the word
+# is a palindrome.
+#
+# Difficulty:
+# 3/10
+#
+# Example:
+# palindrome("hello") should return false
+# palindrome("racecar") should return true
+#
+# Check your solution by running the tests:
+# ruby tests/06_palindrome_test.rb
+#
+
+def palindrome (string)
+  # Your code here
+end
