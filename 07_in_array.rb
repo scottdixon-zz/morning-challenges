07e54eb7e2ed271c74322d0323972f959d7bf7a1
Your Name <renzo-lim@hotmail.com>
2017-10-09 10:09:00 +1000

    Shorter solution added

--- 07_in_array.rb
+++ 07_in_array.rb
@@ -17,11 +17,13 @@
 
 def in_array (needle, haystack)
   # Your code here
-  result = false
-  haystack.each_with_index do |word, index|
-    if needle == haystack[index]
-      result = true
-    end
-  end
-  return result
+  haystack.include? needle
+  # First Attempt
+  # result = false
+  # haystack.each_with_index do |word, index|
+  #   if needle == haystack[index]
+  #     result = true
+  #   end
+  # end
+  # return result
 end
