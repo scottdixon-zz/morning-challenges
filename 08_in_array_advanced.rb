a040c27a7cbd3a4be979d9b3dbaed362e8a4d0f3
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-10-10 16:04:59 +1000

    in_array

--- 08_in_array_advanced.rb
+++ 08_in_array_advanced.rb
@@ -20,6 +20,32 @@
 #
 
 def in_array_advanced (needle, haystack, strict)
-  # Your code here
-  
+  if strict
+    haystack.each { |word| return true if word == needle }
+  else
+    haystack.each { |word| return true if word.downcase == needle.downcase }
+  end
+  return false
 end
+
+# if !strict
+#   haystack.each do |word|
+#     if word != needle
+#       false
+#     end
+#   end
+# end
+
+  # for needle in haystack do
+  #   needle.split([/A-z/]) == needle.downcase
+  #   haystack.downcase == haystack.downcase
+  #   return true
+  # end
+  #
+  # else strict == false
+  #   for needle in haystack do
+  #     needle.scan[/a-z/]
+  #     haystack.scan[/a-z/]
+  #     return false
+  #   end
+  # end
