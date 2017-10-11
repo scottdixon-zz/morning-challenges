82db40b16a451a7c8d5d0b9965097324ab467c97
Scott Dixon <scott@howrefreshing.com.au>
2017-10-11 16:49:46 +1000

    10 - Dictionary

---
+++ 10_dictionary.rb
@@ -0,0 +1,25 @@
+# Dictionary
+#
+# Extend the Dictionary class.
+#
+# It should have three methods:
+# .add_word(word, definition) -> Should store a word and definition
+# .lookup(word) -> Should return a definition
+# .total_words -> Should return the number of words stored
+#
+# Difficulty:
+# 4/10
+#
+# Example:
+# dictionary = Dictionary.new
+# dictionary.add_word('ruby', 'A precious stone')
+# dictionary.total_words -> should return 1
+# dictionary.lookup('ruby') -> should return 'A previous stone'
+#
+# Check your solution by running the tests:
+# ruby tests/10_dictionary_test.rb
+#
+
+class Dictionary
+  # Your code here
+end
