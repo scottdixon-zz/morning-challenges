4b3675b88a06a47ae8c1520505144602c12fbace
Scott Dixon <scott@howrefreshing.com.au>
2017-10-16 06:55:25 +1000

    12 - FizzBuzz

---
+++ 12_fizzbuzz.rb
@@ -0,0 +1,29 @@
+# FizzBuzz
+#
+# Don't look this one up until you complete it yourself!
+# It's a common interview question and there will be plenty
+# of spoilers on the interwebs.
+#
+# This is a trivial question with many simple solutions.
+# Try to write the least amount of code as possible.
+# (AKA: Travisify™ your solution).
+#
+# Write a program that prints the numbers from 1 to 100.
+# But for multiples of three print “Fizz” instead of the
+# number and for the multiples of five print “Buzz”. For
+# numbers which are multiples of both three and five
+# print “FizzBuzz”.
+#
+# Difficulty:
+# 4/10
+#
+# Example:
+# 1
+# 2
+# Fizz
+# 4
+# Buzz
+# ...etc
+#
+
+# Your code here  
