cc9a6f14f45ac6dc9cde4c906600dc41e25c2e8f
calebharnell <calebharnell@hotmail.com>
2017-10-16 09:29:18 +1000

    Monday_Oct_16 solution

--- 12_fizzbuzz.rb
+++ 12_fizzbuzz.rb
@@ -26,4 +26,17 @@
 # ...etc
 #
 
-# Your code here  
+def fizzbuzz
+  range = (1..100).to_a
+  range.each do |x|
+    if x % 3 == 0
+      puts "fizz"
+    elsif x % 5 == 0
+      puts "buzz"
+    else
+      puts x
+    end
+  end
+end
+
+fizzbuzz
