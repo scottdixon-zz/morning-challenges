d5c0d9a624c5afe840ba3614f22e44d9812c08b3
DeadFred <f.cliff1@hotmail.com>
2017-10-16 10:09:18 +1000

    working

--- 12_fizzbuzz.rb
+++ 12_fizzbuzz.rb
@@ -26,4 +26,20 @@
 # ...etc
 #
 
-# Your code here  
+def fizzbuzz
+
+    1..100.times do |num|
+
+    if (num%3) ==0 && (num%5) == 0
+      puts 'FizzBuzz'
+    elsif (num%5) == 0
+      puts 'Buzz'
+    elsif (num%3) == 0
+      puts 'Fizz'
+    else
+      puts num
+    end
+  end
+end
+
+fizzbuzz
