f4263abf1c62d8341136e592d7b2a72f541c65b1
ChristopherMaierInc <mail@christophermaier.com.au>
2017-10-16 10:13:01 +1000

    12_FizzBuzz completed

--- 12_fizzbuzz.rb
+++ 12_fizzbuzz.rb
@@ -26,4 +26,14 @@
 # ...etc
 #
 
-# Your code here  
+(1..100).each do |n|
+  if n % 3 == 0 and n % 5 == 0
+    puts "FizzBuzz"
+  elsif n % 3 == 0
+    puts "Fizz"
+  elsif n % 5 == 0
+    puts "Buzz"
+  else
+    puts n
+  end
+end
