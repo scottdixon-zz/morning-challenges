b14b2af2732bbb5058faa79d210d75318c923e9f
Scott Dixon <scott@howrefreshing.com.au>
2017-10-18 07:59:11 +1000

    15 - Exes And Ohs

---
+++ 14_included_once.rb
@@ -0,0 +1,23 @@
+# Included Once
+#
+# Write a method that takes an array and a string. Return
+# true if the string is found in the array only once.
+# Otherwise return false.
+#
+# What's the time complexity of your solution? Can it be improved?
+#
+# *** Whiteboard this first ***
+#
+# Difficulty: 4/10
+#
+# Example:
+# includedOnce(['hello','hi','hi'], 'hi') -> false
+# includedOnce(['hello','hi','hey'], 'hi') -> true
+#
+# Check your solution by running the tests:
+# ruby tests/14_included_once_test.rb
+#
+
+def includedOnce (haystack, needle)
+  # Your code here
+end
