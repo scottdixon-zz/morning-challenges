ade63f035ba5e75ae19f9462a69b73f65a260382
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-10-23 10:48:03 +1000

    Added solution using Nokogiri gem

--- 16_page_title.rb
+++ 16_page_title.rb
@@ -11,7 +11,10 @@
 # Check your answers by running the tests:
 # ruby tests/16_page_title_test.rb
 #
+require 'nokogiri'
+require 'open-uri'
 
 def pageTitle(url)
-  # your code here
+  page = Nokogiri::HTML(open(url))
+  return page.css("title")[0].text
 end
