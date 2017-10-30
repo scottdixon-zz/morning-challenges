28008db6eb817a2bdc581f531cf27d0375f28748
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-10-30 10:36:32 +1000

    JS Largest Number Solution

--- 17_largest_number.js
+++ 17_largest_number.js
@@ -15,10 +15,7 @@ mocha tests/17_largest_number_test.js
 */
 
 function largestNumber (a, b) {
-  // Your code here
-  // Google JS syntax ;)
+  return Math.max(a, b)
 }
-
-
 // Don't remove this line, the tests need it
 module.exports = largestNumber
