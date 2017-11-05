529c2cc46fabdab9bbe442375aa85df2db2a23ca
Scott Dixon <scott@howrefreshing.com.au>
2017-11-06 06:28:15 +1000

    24 - FizzBuzz (JS edition)

---
+++ 24_fizzbuzz.js
@@ -0,0 +1,23 @@
+// FizzBuzz - JS Edition
+//
+// Practice writing the JS by hand. Avoid copy/paste soluitons.
+// Reference your previous Ruby solution if you get stuck.
+//
+// Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print “Fizz” instead of the
+// number and for the multiples of five print “Buzz”. For
+// numbers which are multiples of both three and five
+// print “FizzBuzz”.
+//
+// Test your solution:
+// $ node 24_fizzbuzz.js
+//
+// Example:
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// ...etc
+
+// Your code here!
