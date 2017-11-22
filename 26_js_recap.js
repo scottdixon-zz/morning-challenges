38b0b146e990e251324107437cacaf7131852053
LorenzoLim <renzo-lim@hotmail.com>
2017-11-22 11:33:11 +1000

    26_js_recap Done

--- 26_js_recap.js
+++ 26_js_recap.js
@@ -1,13 +1,13 @@
 // *Logging*
 // 1. console log 'hello world' to the screen
-
+console.log('hello world');
 
 
 // *Returning*
 // 1. Line 12 logs whatever greet() returns.
 // Right now it's logging undefined. Fix me!
 function greet () {
-  'hello!';
+  return 'hello!';
 }
 console.log(greet());
 
@@ -15,10 +15,11 @@ console.log(greet());
 
 // *Functions & Arrow Functions*
 // Here's a function that takes a first and last name and logs a full name.
-function fullName (first, last) {
+const fullName = (first, last) => {
   console.log(first + ' ' + last);
-}
+};
 // 1. Call the function so it puts your full name onto the screen.
+fullName('Renzo', 'Lim');
 
 // 2. Use modern syntax, turn fullName into an es6 arrow function.
 
@@ -33,15 +34,15 @@ function fullName (first, last) {
 let number_1 = 10;
 let number_2 = 20;
 let number_3 = 30;
-let largestNumber = 0; //Use Math.max here. Keep it simple. Don't use an array.
+let largestNumber = Math.max(number_1, number_2, number_3); //Use Math.max here. Keep it simple. Don't use an array.
 console.log('Largest number is: ' + largestNumber);
 
 
 
 // *Scope*
-let today = 'Tuesday';
+var today = 'Tuesday';
 if (today === 'Tuesday') {
-  let tomorrow = 'Wednesday';
+  var tomorrow = 'Wednesday';
 }
 // Tomorrow is undefined on the line below. Uncomment & fix me! (Hint: let is block scoped)
-// console.log('Tomorrow is ' + tomorrow);
+console.log('Tomorrow is ' + tomorrow);
