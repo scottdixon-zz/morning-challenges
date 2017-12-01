7709d6948074185e67f3771de6cd72929ffd0d10
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-12-01 10:24:40 +1000

    button generator and counter

--- 34_state/src/App.js
+++ 34_state/src/App.js
@@ -1,34 +1,58 @@
 /*
 
   Where should state live?
-  
+
   Get the below app working. It should track how many times
   each button has been clicked.
 
   Hint: Each button should have it's own state.
 
   Beast mode:
-  Add a button "Clik here to add another button"
+  Add a button "Click here to add another button"
   Add a "Total clicks" to the top of the App component.
 
 */
-
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Button from './components/Button'
 import './App.css';
+let currentId = 1
+const genId = () => ++currentId
+
+// let totalClicks = 0
+// const newClicks = () => ++totalClicks
 
 class App extends Component {
+  state = {
+    buttons: [],
+    totalClicks: 0
+  }
+  handleAddButton = () => {
+    this.setState(prevState => ({
+      buttons: [...prevState.buttons, genId()],
+      // totalClicks: prevState.totalClicks + 1
+    }))
+  }
+  handleTotalClicks = () => {
+    this.setState(prevState => ({
+      totalClicks: prevState.totalClicks + 1
+    }))
+  }
+
+
   render() {
     return (
       <div className="App">
-        <button>I have been clicked 0 times</button>
-        <button>I have been clicked 0 times</button>
-        <button>I have been clicked 0 times</button>
-        <button>I have been clicked 0 times</button>
-        <button>I have been clicked 0 times</button>
-        <button>I have been clicked 0 times</button>
-        <button>I have been clicked 0 times</button>
+        <p>total clicks: {this.state.totalClicks}</p>
+        <button onClick={this.handleAddButton}>Add another button please</button>
+        <div>
+          {
+            this.state.buttons.map(id=> <Button key={id}
+                                                parentsHandleTotalClicks={this.handleTotalClicks}
+            />)
+          }
+        </div>
       </div>
+
     );
   }
 }
