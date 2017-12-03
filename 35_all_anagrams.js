2bc81ed6241e722007ac9dbbe7b30cbe54036092
Scott Dixon <scott@howrefreshing.com.au>
2017-12-04 07:13:38 +1000

    35 - All Anagrams

---
+++ 35_all_anagrams.js
@@ -0,0 +1,21 @@
+/*
+  An anagram is a word or a phrase made by rearranging the letters of
+  another word or phrase; for example, “act” is an anagram of “cat”.
+
+  allAnagrams takes a word (a) and an array of potential anagrams (b).
+
+  Return an array of actual anagrams.
+  
+  For example:
+  allAnagrams("cat", ["act","dog","god","tac"]) should return: ["act, tac"]
+
+  Tests:
+  mocha tests/35_anagram_test.js
+
+*/
+
+const allAnagrams = (a, b) => {
+  // Your code here
+}
+
+module.exports = allAnagrams;
