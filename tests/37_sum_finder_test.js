f023ef28c273c36d81e5c2166324708ac3223f08
Scott Dixon <scott@howrefreshing.com.au>
2017-12-06 08:48:42 +1000

    Added better tests to sum finder

--- tests/37_sum_finder_test.js
+++ tests/37_sum_finder_test.js
@@ -7,10 +7,10 @@ describe("Sum Finder", function() {
     assert.equal(sumFinder([1,8,9,5], 6), true);
   });
   it("Should return false if no two numbers add up to sum", function() {
-    assert.equal(sumFinder([1,3], 2), false);
+    assert.equal(sumFinder([1,3], 5), false);
   });
   it("Shouldn't compare the same number to get the total", function() {
-    // e.g. 4+4 is 8.
-    assert.equal(sumFinder([4,5], 8), false);
+    // e.g. 5+5 is 10.
+    assert.equal(sumFinder([4,5], 10), false);
   });
 });
