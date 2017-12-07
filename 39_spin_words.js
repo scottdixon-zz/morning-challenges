e449c7bc9f992977c76bd2887e2feea99dbd1f97
Scott Dixon <scott@howrefreshing.com.au>
2017-12-07 20:06:31 +1000

    39 - Spin Words

---
+++ 39_spin_words.js
@@ -0,0 +1,18 @@
+/*
+
+Spin words (From codewars)
+
+Write a function that takes in a string of one or more words,
+and returns the same string, but with all five or more letter
+words reversed. Strings passed in will consist of only letters
+and spaces. Spaces will be included only when more than one
+word is present.
+
+Examples:
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+spinWords( "This is a test") => returns "This is a test"
+spinWords( "This is another test" )=> returns "This is rehtona test"
+
+Write the tests first and then make your tests pass!
+
+*/
