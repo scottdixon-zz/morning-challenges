e32aba80801db81f64e2120a2d34e5cdea8f6582
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-12-12 09:27:45 +1000

    stageone

---
+++ 41_callbacks_snipett.js
@@ -0,0 +1,57 @@
+//https://gist.github.com/scottdixon/ce988c99b67ddd9f7678cca7be89828c
+// CHALLENGE
+// ---------
+//
+// https://gist.github.com/scottdixon/ce988c99b67ddd9f7678cca7be89828c
+//
+// Line 13 is calling 'sayGreetings' and handing it a callback 'sayNiceToMeetYou'
+// Line 14 is calling 'sayGreetings' and handing it a callback 'sayGoodbye'
+//
+// Fix the 'sayGreetings' function so it runs the callback each time.
+//
+//
+//
+//
+//
+// DESIRED OUTPUT:
+// Greetings!
+// Nice to meet you!
+// Greetings!
+// Goodbye!
+
+
+//
+// const sayGreetings = (greeting) => {
+//   console.log('Greetings!')
+//   greeting()
+// }
+//
+// const sayNiceToMeetYou = () => {
+//   console.log('Nice to meet you!')
+// }
+//
+// const sayGoodbye = () => {
+//   console.log('Goodbye!')
+// }
+//
+// sayGreetings(sayNiceToMeetYou)
+// sayGreetings(sayGoodbye)
+
+
+// Remind yourself how .filter works. Build it yourself using only .forEach.
+// We've started you off.
+
+const filter = (items, callback) => {
+  const results = []
+  items.forEach((callback) => {
+    if (callback === true)
+    results.push(callback)
+  })
+  console.log(results)
+}
+
+// I'm using your Filter method to return even numbers.
+// If your filter method is correct, this code should return: [2, 4]
+filter([1,2,3,4], (item) => {
+  return item % 2 === 0
+})
