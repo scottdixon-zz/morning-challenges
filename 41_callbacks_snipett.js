3687cd54726f1311c8ea1ac4c139b56b1f83d3d8
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-12-12 10:08:22 +1000

    callback Hell

--- 41_callbacks_snipett.js
+++ 41_callbacks_snipett.js
@@ -43,15 +43,16 @@
 
 const filter = (items, callback) => {
   const results = []
-  items.forEach((callback) => {
-    if (callback === true)
-    results.push(callback)
+  items.forEach((item) => {
+    if (callback(item)) {
+      results.push(item)
+    }
   })
   console.log(results)
 }
 
 // I'm using your Filter method to return even numbers.
 // If your filter method is correct, this code should return: [2, 4]
-filter([1,2,3,4], (item) => {
-  return item % 2 === 0
+filter([1,2,3,4], (num) => {
+  return num % 2 === 0
 })
